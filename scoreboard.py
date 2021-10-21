329a3a8c59a9350b374d6968618b5a6e1a0cfdc1
dvisionst <62859441+dvisionst@users.noreply.github.com>
2021-10-21 16:07:49 -0700

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 12, "bold")
 MOVE = False
@@ -6,26 +7,37 @@ MOVE = False
 
 class Scoreboard(Turtle):
 
-    def __init__(self):
+    def __init__(self, name):
         super().__init__()
         self.penup()
         self.goto(0, 280)
         self.color("white")
         self.tally = 0
+        self.current_user = name
+        with open("data.txt") as file:
+            contents = file.read()
+        self.high_score = int(contents[0])
+        self.user_high = contents[2:]
         self.hideturtle()
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.tally} ", move=MOVE, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"{self.current_user} your score: {self.tally} "
+                   f"High: {self.high_score} by {self.user_high}",
+                   move=MOVE, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", move=MOVE, align=ALIGNMENT, font=FONT)
+    def reset_s(self):
+        if self.tally > self.high_score:
+            self.high_score = self.tally
+            self.user_high = self.current_user
+            with open("data.txt", mode="w") as nfile:
+                nfile.write(f"{self.high_score} {self.user_high}")
+        self.tally = 0
+        self.update_score()
 
     def score(self):
         self.tally += 1
-        self.clear()
         self.update_score()
-
 
 
